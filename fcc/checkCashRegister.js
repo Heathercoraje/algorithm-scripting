9abdef6e1fafca61a95bcbd7ccd3cd60eee41e6b
heathercoraje <yhk7830@hotmail.com>
2018-03-01 12:21:25 +0900

    adding checkCashRegister from fcc and test

--- fcc/checkCashRegister.js
+++ fcc/checkCashRegister.js
@@ -1,31 +1,37 @@
 function checkCashRegister(price, cash, cid) {
-  var change = (cash - price) * 100;
-  console.log("??", change);
-  var currency = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
-  var value = 0;
-  var changeRecord = [];
-  // muatate cid?
-  cid.forEach(el => (el[1] = Math.round(el[1] * 100)));
+	var change = (cash - price) * 100;
+	var currency = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
+	var changeRecord = [];
+	var value;
 
-  console.log(cid);
+	// mutate cid money to have same unit with currency and change
+	cid.forEach(el => (el[1] = Math.round(el[1] * 100)));
 
-  function enoughFund(cid) {
-    var sum = cid.filter((el, i) => change >= currency[i]);
-    return sum.reduce((a, b) => {
-      return a + b[1];
-    }, 0);
-  }
-  console.log(enoughFund(cid));
-  console.log("?", change);
+	//calcuate total money to return string for 2 cases
+	function enoughFund(cid) {
+		var sum = cid.reduce((a, b) => a + b[1], 0);
+		return sum;
+	}
+
+	// first, check 2 string cases
+	if (enoughFund(cid) < change) {
+		return 'Insufficient fund';
+	} else if (enoughFund(cid) === change) {
+		return 'Closed';
+	} else {
+		for (var i = cid.length - 1; i > -1; i--) {
+			value = 0;
+			while (cid[i][1] > 0 && change >= currency[i]) {
+				change -= currency[i];
+				cid[i][1] -= currency[i];
+				value += currency[i];
+			}
+			if (value) {
+				changeRecord.push([cid[i][0], value]);
+			}
+		}
+	}
+	return changeRecord;
 }
-checkCashRegister(19.5, 20.0, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90.0],
-  ["FIVE", 55.0],
-  ["TEN", 20.0],
-  ["TWENTY", 60.0],
-  ["ONE HUNDRED", 100.0]
-]);
+
+module.exports = checkCashRegister;
