fb1a9c53a2da8ebed64966f17276eb015a769ab5
heathercoraje <yhk7830@hotmail.com>
2018-03-13 12:41:38 +0900

    adding permAlone code

--- fcc/permAlon.js
+++ fcc/permAlon.js
@@ -6,30 +6,21 @@ function permAlone(str) {
 		var temp = arr[a];
 		arr[a] = arr[b];
 		arr[b] = temp;
-		console.log('swap', arr.join(''));
 	}
 
 	function generator(n) {
-		console.log('hola');
 		if (n === 1 && !/([a-z])\1+/.test(arr.join(''))) {
 			container.push(arr.join(''));
 			counter++;
 		} else {
-			console.log('one loop');
 			for (var i = 0; i !== n; i++) {
-				console.log('generator');
-				console.log('i', i);
-				console.log('n', n);
 				generator(n - 1);
 				swap(n % 2 ? 0 : i, n - 1);
 			}
 		}
-
-		console.log(container);
-		console.log('counter', counter);
 	}
 	generator(arr.length);
-	console.log('counter', counter);
 	return counter;
 }
-permAlone('abc');
+//test
+//permAlone('abc');
