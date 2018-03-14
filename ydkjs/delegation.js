d45c6d11984b7ef42cabad1007ea131beb418e76
heathercoraje <yhk7830@hotmail.com>
2018-03-14 11:08:56 +0900

    adding first test for delelegation

---
+++ ydkjs/delegation.js
@@ -0,0 +1,28 @@
+var task = {
+	setID: function setId(ID) {
+		this.id = ID;
+	},
+	outputID: function outputID() {
+		console.log(this.id);
+	}
+};
+
+// make 'ABC' delegate to task
+var ABC = Object.create(task);
+
+ABC.prepareTask = function(ID, label) {
+	this.setID(ID);
+	this.label = label;
+};
+
+ABC.outputTaskDetail = function() {
+	this.outputID();
+	console.log(this.label);
+};
+
+//ABC.prepareTask(1, 'COSTCO'); // output 1 using prototype chanin task.setID
+//ABC.outputTaskDetail(); // output COSTCO using prototype chain task.outputID
+
+// what we really care about is that ABC delegates to task object via Object.create(..)
+
+module.exports = task;
